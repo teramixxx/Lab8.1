44db45957e8aa91822569dcfecc9235cf963245d
teramixxx <127118011+teramixxx@users.noreply.github.com>
2023-04-03 14:22:47 +0300

    Adapter

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace DoFactory.GangOfFour.Adapter.Structural
+{
+
+    class MainApp
+    {
+
+        static void Main()
+        {
+
+            Target target = new Adapter();
+            target.Request();
+
+            Console.ReadKey();
+        }
+    }
+
+
+    class Target
+    {
+        public virtual void Request()
+        {
+            Console.WriteLine("Визваний помічник");
+        }
+    }
+
+    class Adapter : Target
+    {
+        private Adaptee _adaptee = new Adaptee();
+
+        public override void Request()
+        {
+
+            _adaptee.SpecificRequest();
+        }
+    }
+
+
+    class Adaptee
+    {
+        public void SpecificRequest()
+        {
+            Console.WriteLine("Помiчник прийшов");
+        }
+    }
+}
